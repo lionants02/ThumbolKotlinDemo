b48ae71488cac0f266bf97f6fcd6e0fb4b33976e
MaxPC_NECTEC <singmax161@gmail.com>
2017-08-15 16:48:51 +0700

    Init Library

--- app/src/main/java/nstda/or/th/thumbolkotlindemo/ImageView.java
+++ app/src/main/java/nstda/or/th/thumbolkotlindemo/ImageView.java
@@ -3,6 +3,7 @@ package nstda.or.th.thumbolkotlindemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class ImageView extends AppCompatActivity {
 
     @Override
