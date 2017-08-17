6aa71098b58b8e82209d66f7035c26ba80adeae4
MaxPC_NECTEC <singmax161@gmail.com>
2017-08-17 16:44:44 +0700

    Real Run

---
+++ app/src/main/java/nstda/or/th/thumbolkotlindemo/ImageView.kt
@@ -0,0 +1,18 @@
+package nstda.or.th.thumbolkotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import th.or.nectec.*
+
+class ImageView : AppCompatActivity() {
+    var btBlur :Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_view)
+
+
+    }
+
+}
